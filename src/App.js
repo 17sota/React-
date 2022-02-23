bc52442fcd12176b4afd3c6a6e2dc1cbe788d31d
kamise sota <85654815+17sota@users.noreply.github.com>
2022-02-23 22:36:30 +0900

    Update App.js
    更新

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+
+
+const App = function () {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const queryWeather = () => {
+
+    fetch('https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json')
+      .then((response) => response.json())
+      .then((weather) => {
+
+        setData(weather[0].timeSeries[0].areas[0]);
+        setLoading(false);
+      });
+  };
+  useEffect(() => {
+
+    queryWeather();
+  }, []);
+  let weatherInfo;
+  if (loading) {
+
+    weatherInfo = <p>loading</p>;
+  } else {
+    weatherInfo = (
+      <p>
+        {data.area.name}の明日の天気 {data.weathers[0]}
+      </p>
+    );
+  }
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <h1>天気</h1>
+      {weatherInfo}
+
+      
+    </>
   );
-}
+};
 
 export default App;
